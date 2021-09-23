7f7c1bc79917d2fd5a082f77a472483536d1c60e
CeleZ-Sensei <CeleZ.Neiva@Gmail.com>
2021-09-23 19:58:39 +0700

    23/09/21 - #01, #02

---
+++ What is Associative array/asarr.js
@@ -0,0 +1,14 @@
+// https://www.twitch.tv/videos/1147701868 <<< ดูไปด้วยนะ
+
+var arr = ["Ball", "Book", "Pen", "cloak of invisibility", "bah bah"];
+// console.log(arr[3]);
+
+var asarr = {"PBass":"Ball", "IBoss":"Book", "HappyP0628":"Pen", "jeffyntpth":"COI", "PixieJun":"bah bah"};
+// console.log(asarr["HappyP0628"]);
+
+var xixnus = {"gender":"male", "age":"45", "food":"pizza"};
+// console.log(user)
+
+var user = {"xixnus" : {"gender":"male", "age":"45", "food":"pizza"},
+            "sensei" : {"gender":"male", "age":"220", "food":"rich"}};
+// console.log(user["xixnus"]["age"]);
---
+++ What is this/thisis.js
@@ -0,0 +1,31 @@
+// var user = {
+//     name: "celez",
+//     lastname: "neiva",
+//     age: "225",
+//     compile() {
+//         console.log(this);
+//     }
+// }
+
+// user.compile();
+
+// var user = {
+//     name: "celez",
+//     lastname: "neiva",
+//     age: "225",
+// }
+
+// function thisis(){
+//     console.log(this.name +" "+ this.lastname +" "+ this.age);
+// }
+
+// thisis.call(user);
+
+
+var user = {
+    name: "celez",
+    lastname: "neiva",
+    age: "225",
+}
+
+console.log(user.name);
